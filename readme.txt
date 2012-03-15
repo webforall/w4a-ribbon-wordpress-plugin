f4bae2cc5ff1234d18f7ddb178ae668d132606d5
Francesco Tassi <ftassi@rfc-1918.(none)>
2012-03-15 19:39:54 +0100

    commit iniziale. TODO Rendere personalizzabile ribbon utilizzato e link di destinazione

---
+++ readme.txt
@@ -0,0 +1,14 @@
+=== WebForAll Ribbon ===
+Contributors: ftassi
+Tags: w4a, social, no-profit
+Requires at least: 3.0
+Tested up to: 3.3
+Stable tag: 0.1
+License: GPLv2 or later
+
+WebForAll ribbon add out ribbon to your wordpress website
+
+== Description ==
+
+WebForAll ribbon add out ribbon to your wordpress website. The ribbon will be 
+linked to http://webforall-project.it/
---
+++ w4a-ribbon.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @package w4a-ribbon
+ * @version 0.1
+ */
+/*
+  Plugin Name: WebForAll Ribbon
+  Plugin URI: http://wordpress.org/extend/plugins/w4a-ribbon/
+  Description: Adds WebForAll ribbon to your wordpress blog
+  Author: ftassi <tassi.francesco@gmail.com>
+  Version: 0.1
+  Author URI: http://flavors.me/ftassi
+ */
+
+$w4aRibbon = new W4aRibbon();
+
+class W4aRibbon
+{
+
+  const VERSION = '0.1';
+
+  function __construct()
+  {
+    add_action('wp_footer', array($this, 'printRibbon'));
+    add_action('wp', array($this, 'loadCss'));
+  }
+
+  public function printRibbon()
+  {
+    $ribbonPath = plugins_url('w4a-ribbon/images/6ribbon-without.png');
+    ob_start();
+    require_once( dirname(__FILE__) . '/template/ribbon.php' );
+    echo ob_get_clean();
+  }
+
+  public function loadCss()
+  {
+    wp_register_style('w4a-ribbon-style', plugins_url('css/style.css', __FILE__), false, self::VERSION);
+    wp_enqueue_style('w4a-ribbon-style');
+  }
+
+}
